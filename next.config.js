4f2caac1f596fe8405efbcf04603dcdbbef24701
haouarihk <haithem2001@gmail.com>
2025-04-08 18:50:02 +0100

    build a standalone app

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const IsStandalone = process.env.BUILD_STANDALONE == "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: IsStandalone ? "standalone" : undefined,
 };
 
 module.exports = nextConfig;
